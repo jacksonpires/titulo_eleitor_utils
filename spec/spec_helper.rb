1a096848462c372a0398c42eccbc46c2feb240e3
Jackson Pires <jackson.pires@gmail.com>
2014-04-04 09:02:36 -0300

    Adicionando o Coderalls.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,12 @@ require "codeclimate-test-reporter"
 # Starts Code Climate Test Reporter
 CodeClimate::TestReporter.start
 
+# Require Coveralls for Test cover
+require 'coveralls'
+
+# Starts Coderalls
+Coveralls.wear!
+
 # Require other files of project
 require "titulo_eleitor_utils"
 require_relative "../lib/titulo_eleitor_utils/titulo_eleitor"
